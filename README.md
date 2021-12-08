4c33d07e4c9c353b3bb045d36d4cb6e4d2d06c6d
Nate Foss <npfoss@gmail.com>
2021-12-07 22:05:09 -0500

    everything

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# mod-spotify-as-status
+# Gather mod example: set status to Spotify
+
 a Gather mod which sets your status to your currently playing track on spotify
+
+check out the [websocket API docs](https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063) for more!
+
+## setup
+
+prereq: have NodeJS and npm installed
+
+run `npm install`
+
+put your Gather and Spotify API keys in a file named `api-key.js` like so:
+
+```js
+export const API_KEY = "your-api-key-here";
+export const SPOTIFY_KEY = "your-very-long-spotify-token-here";
+```
+
+get the gather one here: https://gather.town/apiKeys
+and spotify here: https://developer.spotify.com/console/get-users-currently-playing-track/
+
+replace the `SPACE_ID` in index.ts with your own spaceId
+
+## running
+
+`npm run start`
---
+++ index.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { API_KEY, SPOTIFY_KEY } from "./api-key";
+import { Game } from "@gathertown/gather-game-client";
+global.WebSocket = require("isomorphic-ws");
+
+const SPACE_ID = "oxrhEtb3sV7VutbQ\\GatherOffice";
+
+// setup
+
+const game = new Game(() => Promise.resolve({ apiKey: API_KEY }));
+game.connect(SPACE_ID); // replace with your spaceId of choice
+game.subscribeToConnection((connected) => console.log("connected?", connected));
+
+//
+
+// check every 5s
+
+setInterval(async () => {
+	const res = await axios.get(
+		"https://api.spotify.com/v1/me/player/currently-playing",
+		{
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${SPOTIFY_KEY}`,
+			},
+		}
+	);
+
+	console.log(res.data.item.name);
+
+	game.engine.sendAction({
+		$case: "setEmojiStatus",
+		setEmojiStatus: {
+			emojiStatus: "🎶",
+		},
+	});
+	game.engine.sendAction({
+		$case: "setTextStatus",
+		setTextStatus: {
+			textStatus: res.data.item.name,
+		},
+	});
+}, 5000);
